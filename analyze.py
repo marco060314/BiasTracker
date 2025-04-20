d4be95bbe01b8253e893da0b98d8c6a3d89ed159
Marco Wang <marco060314@gmail.com>
2025-04-20 10:45:11 -0500

    lexicons, analyze for lexicons and polarity

--- analyze.py
+++ analyze.py
@@ -7,14 +7,16 @@ from transformers import pipeline
 from nltk.tokenize import sent_tokenize
 import spacy
 from spacy.tokens import DocBin
+from lexicons import HEDGE_WORDS, WEASEL_WORDS, BUZZWORDS_WORDS, EMOTION_WORDS
+from spacy.matcher import PhraseMatcher
 
 
 classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 
 def sentiment_analysis(article):
-    sentences = sent_tokenize(article)
+    sentences = sent_tokenize(article[0]["article"])
     results = classifier(sentences)
-    
+    #add polarity for headline
     polarity = 0
     polarity_list = []
     
@@ -37,15 +39,76 @@ def sentiment_analysis(article):
 
 #spaCy section
 def bias_analysis(article):
-    nlp = spacy.load("en_core_web_md")
+    sentences = sent_tokenize(article[0]["article"])
+    nlp = spacy.load("en_core_web_sm")
     title_doc = nlp(article[0]["headline"])
-    doc = nlp(article[0]["article"])
+    BATCH_SIZE = 8
+    docs = list(nlp.pipe(sentences, batch_size=BATCH_SIZE)) #batch processing size
+    word_count = len(article[0]["article"].split())
+    emotion_count = 0
+    weasel_count = 0
+    buzzwords_count = 0
+    hedge_count = 0
+
+    for sent in docs.sents:
+        for token in sent:
+            if token.lemma_.lower() in EMOTION_WORDS:
+                emotion_count += 1
+            if token.lemma_.lower() in WEASEL_WORDS:
+                weasel_count += 1
+            if token.lemma_.lower() in HEDGE_WORDS:
+                hedge_count += 1
+            if token.lemma_.lower() in BUZZWORDS_WORDS:
+                buzzwords_count += 1
+    metrics = {
+        "active": active,
+        "passive": passive,
+        "agent": agent,
+        "emotion": emotion_count,
+        "weasel": weasel_count,
+        "hedge": hedge_count,
+        "buzzwords": buzzwords_count
+        }
+    return metrics
+
+
     
+#check for passive voice vs active voice, and check for when the agent is omitted
+def agent_analysis(docs):
+    active_count = 0
+    passive_count = 0
+    omitted_agent_count = 0
+    for sent in docs.sents:
+        passive = False
+        agent = False
+        nsubj = False
+        nsubjpass = False
+        auxpass = False
+    
+        for token in sent:
+            if token.dep_ == "nsubj":
+                nsubj = True
+            if token.dep_ == "nsubjpass":
+                nsubjpass = True
+            if token.dep_ == "auxpass":
+                auxpass = True
+            if token.dep_ == "agent":
+                for child in token.children:
+                    if child.dep_ == "pobj":
+                        agent = True
+    if nsubjpass and auxpass:
+        passive = True
+        passive_count += 1
+        if not agent:
+            omitted_agent_count += 1
+    elif nsubj:
+        active_count += 1
 
-
-
+    return active_count, passive_count, omitted_agent_count
 
 
 #misinformation section
+def misinformation_analysis(article):
 
+    return 0
 
---
+++ lexicons.py
@@ -0,0 +1,22 @@
+import os
+from spacy.lang.en import English
+
+BASE_DIR = os.path.dirname(__file__)
+nlp = English()
+def load_wordlist(filename):
+    with open(os.path.join(BASE_DIR, "lexicons", filename), "r") as f:
+        return [nlp.make_doc(phrase.strip().lower()) for phrase in f]
+
+def load_nrc_emotions(filename):
+    emotion_words = []
+    with open(os.path.join(BASE_DIR, "lexicons", filename), "r") as f:
+        for line in f:
+            word, emotion, value = line.strip().split('\t')
+            if int(value) == 1:
+                emotion_words.append(nlp.make_doc(word.lower()))
+    return emotion_words
+
+HEDGE_WORDS = load_wordlist("hedges.txt")
+WEASEL_WORDS = load_wordlist("weasel.txt")
+BUZZWORDS_WORDS = load_wordlist("buzzwords.txt")
+EMOTION_WORDS = load_nrc_emotions("NRC_Emotion_Lexicon.txt")
