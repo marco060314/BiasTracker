a291f7c40e16c04b8f48578c52a41e524f6eeaa6
Marco Wang <marco060314@gmail.com>
2025-04-21 19:02:01 -0500

    attempt to implement streamlit
    need to do some reactor thing

--- streamlit_placeholder.py
+++ streamlit_placeholder.py
@@ -9,10 +9,11 @@ def run_program(url):
     metrics = analyze.bias_analysis(docs)
     active_count, passive_count, agent_omitted_count = analyze.agent_analysis(docs)
     #misinfo = analyze.misinformation_analysis(docs)
-    print("scraped headline:", article[0]["headline"])
-    print(avg_polarity)
-    print(metrics)
+   #print("scraped headline:", article[0]["headline"])
+    #print(avg_polarity)
+    #print(metrics)
     #print(misinfo)
+    return avg_polarity, std_dev, metrics
 
-run_program("https://www.foxnews.com/media/supreme-court-consider-whether-parents-can-opt-out-kids-reading-lgbtq-books-classroom")
+#run_program("https://www.foxnews.com/media/supreme-court-consider-whether-parents-can-opt-out-kids-reading-lgbtq-books-classroom")
 
